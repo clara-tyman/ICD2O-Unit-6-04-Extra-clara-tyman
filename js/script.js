ebf204003120036f0cf968711ed887d4def18cb5
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-03-28 18:20:17 +0000

    storyboard html css js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Clara Tyman  All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Clara Tyman
+// Created on: March 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // input
+  const base1 = parseInt(document.getElementById("base-of-trapezoid-a").value)
+  const base2 = parseInt(document.getElementById("base-of-trapezoid-b").value)
+  const height = parseInt(document.getElementById("height-of-trapezoid").value)
+  // process
+  const area = ((base1 + base2) * height) / 2
+
+  // output
+  document.getElementById("Area-of-a-Trapezoid").innerHTML = area.toFixed(2) + " cm²"
+
+}
