0581cb522a8c04dbf6cab1906e2235ab9a84d877
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-05-27 18:42:54 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -4,15 +4,34 @@
 // Created on: March 2024
 // This file contains the JS functions for index.html
 
-function myButtonClicked() {
+"use strict"
+
+window.onload = function () {
+  // this calculates area of a trapzoid
+
+  const params = new URLSearchParams(document.location.search)
+
+
   // input
-  const base1 = parseInt(document.getElementById("base-of-trapezoid-a").value)
-  const base2 = parseInt(document.getElementById("base-of-trapezoid-b").value)
-  const height = parseInt(document.getElementById("height-of-trapezoid").value)
+  const base1 = parseFloat(params.get('base1'))
+  console.log(base1)
+  const base2 = parseFloat(params.get('base2'))
+  console.log(base2)
+  const height = parseFloat(params.get('height'))
+  console.log(height)
+
   // process
-  const area = ((base1 + base2) * height) / 2
+  const area = ((base1 + base2) / 2) * height
+  const dimensions =
+    "<ul>\n<li>base1 = " +
+    base1 +
+    "</li>\n<li>base2 = " +
+    base2 +
+    "</li>\n<li>height = " +
+    height +
+    "</li>\n</ul>"
 
   // output
-  document.getElementById("Area-of-a-Trapezoid").innerHTML = area.toFixed(2) + " cm²"
-
+  document.getElementById("dimensions").innerHTML = dimensions
+  document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
 }
